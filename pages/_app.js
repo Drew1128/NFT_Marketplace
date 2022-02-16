2a030a59ffbbde75d96bda5ecdbb6dd4b08ab424
LiquidAcre <drewm1128@gmail.com>
2022-02-16 13:06:14 -0600

    Deployed contracts on local enviroment and updated styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function myApp({ Component, pageProps }) {
+  return (
+    <div >
+      <nav className="border-b p-6 bg-gradient-to-r from-sky-500 to-indigo-500">
+        <p className="text-3xl text-white font-bold">Metaverse Marketplace</p>
+        
+
+        
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-blue-200">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-blue-200">
+              Sell Digital Asset
+            </a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-blue-200">
+              My Digital Assets
+            </a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-blue-200">
+              Creator Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
-export default MyApp
+export default myApp
