dbaf2eef4421c3cfcfc6472f13faa589c14c9d28
LiquidAcre <drewm1128@gmail.com>
2022-02-16 13:28:49 -0600

    Change the background color in index.js

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function Home() {
 
   
   return (
-    <div className="flex justify-center bg-zinc-600">
+    <div className="flex justify-center">
       <div className="px-4" style={{ maxWidth: '1600px' }}>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           {
