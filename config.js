77403a33c23a6900ed392185aeb197b4178b8937
Drew Morrison <drewm1128@gmail.com>
2022-04-10 06:42:10 -0500

    deoplyed new contracts on mumbai

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0xccd575DfEe14707f10162dD97f59E7721d18926F"
-export const nftaddress = "0x080BeC288E8eB93604a8674C5E4a0996d6ec27F7"
+export const nftmarketaddress = "0x74EbC5908F058dB9d0705Db8b168E1A6E24b567B"
+export const nftaddress = "0xE3A1f37df3B92d4b856806F0519756A8ed56434c"
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,19 +3,20 @@ const fs = require("fs");
 const privateKey = fs.readFileSync(".secret").toString();
 
 module.exports = {
+ defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 1337
     },
-    mumbai: {
+     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/ac8b909387d344b0850e066a2b944424`,
       accounts: [privateKey]
     },
-    mainnet: {
+   /* mainnet: {
       url: `https://polygon-mainnet.infura.io/v3/${process.env.projectId}`,
       accounts: [privateKey]
     },
-    
+    */
   },
   solidity: "0.8.4",
 };
